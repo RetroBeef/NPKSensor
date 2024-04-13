d17e9e4537ebcae669d37ac433f7a169113a52b3
c3pp0 <cepnet@gmail.com>
2024-04-13 11:16:43 +0200

    initial commit

---
+++ NPKSensor.cpp
@@ -0,0 +1,65 @@
+#include "NPKSensor.h"
+#include "crc.h"
+
+namespace{
+  void interpret_npk_response(const npk_response_t *res, npk_data_t *interp) {
+      interp->soilTemperature = (int16_t)((res->reg0DataHigh << 8) | res->reg0DataLow) / 10.0;
+      interp->soilMoisture = ((res->reg1DataHigh << 8) | res->reg1DataLow) / 10.0;
+      interp->soilSalinity = (int16_t)((res->reg2DataHigh << 8) | res->reg2DataLow);
+      interp->soilConductivity = (int16_t)((res->reg3DataHigh << 8) | res->reg3DataLow);
+      interp->pH = (int16_t)((res->reg4DataHigh << 8) | res->reg4DataLow) / 100.0;
+      interp->soilNitrogenContent = (int16_t)((res->reg5DataHigh << 8) | res->reg5DataLow);
+      interp->soilPhosphorus = (int16_t)((res->reg6DataHigh << 8) | res->reg6DataLow);
+      interp->soilPotassiumContent = (int16_t)((res->reg7DataHigh << 8) | res->reg7DataLow);
+  } 
+}
+
+NPKSensor::NPKSensor(const uint8_t& address, const uint8_t& pinRx, const uint8_t& pinTx) : pinRx(pinRx), pinTx(pinTx){
+  Serial1.begin(9600, SERIAL_8N1, pinRx, pinTx);
+  npkRequest.address = address;
+}
+
+int8_t NPKSensor::update(npk_data_t& npkData){
+  static uint8_t response_data[sizeof(npk_response_t)];
+  static uint16_t expected_crc, received_crc;
+  Serial1.write((uint8_t*)&npkRequest, sizeof(npk_request_t));
+  ssize_t total_bytes_read = 0;
+  while (total_bytes_read < sizeof(npk_response_t)) {
+    ssize_t bytes_read = Serial1.read(response_data + total_bytes_read, sizeof(npk_response_t) - total_bytes_read);
+    if (bytes_read < 0) {
+      Serial.println("Error reading from serial port");
+      return -1;
+    }
+    total_bytes_read += bytes_read;
+  }
+
+  // Check CRC16
+  expected_crc = (response_data[sizeof(npk_response_t) - 2] << 8) | response_data[sizeof(npk_response_t) - 1];
+  received_crc = __builtin_bswap16(calculate_crc16(response_data, sizeof(npk_response_t) - 2));
+
+  if (expected_crc != received_crc) {
+    Serial.printf("CRC16 check failed! Expected: 0x%04X, Received: 0x%04X\n", expected_crc, received_crc);
+    return -1;
+  }
+
+  interpret_npk_response((npk_response_t*)response_data, &npkData);
+  return 0;
+}
+
+String NPKSensor::toJSON(const npk_data_t& npkData) {
+    String jsonStr = "{";
+    jsonStr += "\"add\":" + String(npkRequest.address) + ",";
+    jsonStr += "\"fun\":" + String(npkRequest.function) + ",";
+    jsonStr += "\"soil\":{";
+    jsonStr += "\"temp\":{\"v\":" + String(npkData.soilTemperature, 1) + ",\"u\":\"°C\"},";
+    jsonStr += "\"hum\":{\"v\":" + String(npkData.soilMoisture, 1) + ",\"u\":\"%rH\"},";
+    jsonStr += "\"sal\":{\"v\":" + String(npkData.soilSalinity) + ",\"u\":\"µS/cm\"},";
+    jsonStr += "\"EC\":{\"v\":" + String(npkData.soilConductivity) + ",\"u\":\"µS/cm\"},";
+    jsonStr += "\"pH\":{\"v\":" + String(npkData.pH, 1) + ",\"u\":\"-log(H+)\"},";
+    jsonStr += "\"N\":{\"v\":" + String(npkData.soilNitrogenContent) + ",\"u\":\"mg/kg\"},";
+    jsonStr += "\"P\":{\"v\":" + String(npkData.soilPhosphorus) + ",\"u\":\"mg/kg\"},";
+    jsonStr += "\"K\":{\"v\":" + String(npkData.soilPotassiumContent) + ",\"u\":\"mg/kg\"}";
+    jsonStr += "}}";
+
+    return jsonStr;
+}
---
+++ NPKSensor.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <Arduino.h>
+#include <stdint.h>
+
+#pragma pack(push,1)
+typedef struct {
+  uint8_t address;
+  uint8_t function;
+  uint8_t startRegisterHigh;
+  uint8_t startRegisterLow;
+  uint8_t registerLengthHigh;
+  uint8_t registerLengthLow;
+  uint8_t crcLow;
+  uint8_t crcHigh;
+} npk_request_t;
+
+typedef struct {
+  uint8_t address;
+  uint8_t function;
+  uint8_t dataLength;
+  uint8_t reg0DataHigh;
+  uint8_t reg0DataLow;
+  uint8_t reg1DataHigh;
+  uint8_t reg1DataLow;
+  uint8_t reg2DataHigh;
+  uint8_t reg2DataLow;
+  uint8_t reg3DataHigh;
+  uint8_t reg3DataLow;
+  uint8_t reg4DataHigh;
+  uint8_t reg4DataLow;
+  uint8_t reg5DataHigh;
+  uint8_t reg5DataLow;
+  uint8_t reg6DataHigh;
+  uint8_t reg6DataLow;
+  uint8_t reg7DataHigh;
+  uint8_t reg7DataLow;
+  uint8_t crcLow;
+  uint8_t crcHigh;
+} npk_response_t;
+#pragma push(pop)
+
+typedef struct {
+  float soilTemperature;          // Soil temperature in °C
+  float soilMoisture;             // Soil moisture percentage
+  int soilSalinity;               // Soil salinity value
+  int soilConductivity;           // Soil conductivity value in µS/cm
+  float pH;                       // pH value
+  int soilNitrogenContent;        // Soil nitrogen content in ppm (mg/kg)
+  int soilPhosphorus;             // Soil phosphorus content in ppm (mg/kg)
+  int soilPotassiumContent;       // Soil potassium content in ppm (mg/kg)
+} npk_data_t;
+
+class NPKSensor {
+  protected:
+    npk_request_t npkRequest = {0x01, 0x03, 0x00, 0x00, 0x00, 0x08, 0x44, 0x0c};
+    uint8_t pinRx;
+    uint8_t pinTx;
+  public:
+    NPKSensor(const uint8_t& address, const uint8_t& pinRx, const uint8_t& pinTx);
+    virtual ~NPKSensor() {}
+    int8_t update(npk_data_t& npkData);
+    String toJSON(const npk_data_t& npkData);
+};
