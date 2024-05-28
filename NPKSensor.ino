0c37108388d295a8d947a7214ef6852eb4cbc9d8
c3pp0 <cepnet@gmail.com>
2024-05-28 23:02:49 +0200

    added more ble devices

--- NPKSensor.ino
+++ NPKSensor.ino
@@ -13,11 +13,15 @@
 
 #include <TP357.h>
 #include <Aroma.h>
+#include <FC3000.h>
+#include <RbBleDevice.h>
 
 #include <vector>
 
 #include "NPKSensor.h"
 #include "creds.h"
+
+StaticJsonDocument<64> doc;
 
 typedef struct {
   String deviceName;
@@ -29,12 +33,45 @@ typedef struct {
   float current;
 } TasmotaState_t;
 
-TasmotaState_t lastTasmotaState;
+typedef struct{
+  uint8_t state;
+  uint8_t dimmPercentage;
+  uint32_t localTime;
+  uint32_t lastSeenMs;
+}fc3000_state_t;
+
+typedef struct{
+  uint16_t pan;
+  uint16_t tilt;
+  uint32_t distance;
+  uint32_t lastSeenMs;
+}distance_state_t;
+
+typedef struct{
+  float weight;
+  uint32_t lastSeenMs;
+}weight_state_t;
+
+TasmotaState_t lastTasmotaExhaustionState;
+TasmotaState_t lastTasmotaLightState;
+fc3000_state_t lastLightState = {0,0,0,0};
+distance_state_t lastDistanceState = {0,0,0,0};
+weight_state_t lastWeightState = {0.f,0};
 
 uint8_t scanTime = 5;
 BLEScan* pBLEScan = 0;
-BLEClient* bleClient = 0;
+
+BLEClient* bleClientAroma = 0;
 Aroma* aroma = 0;
+
+BLEClient* bleClientLight = 0;
+FC3000* light = 0;
+
+BLEClient* bleClientDistance = 0;
+RbBleDevice* distance = 0;
+
+BLEClient* bleClientWeight = 0;
+RbBleDevice* weight = 0;
 
 char restToken[25];
 
@@ -42,6 +79,7 @@ std::vector<ScannableBleDevice*> scannableBleDevices;
 ThermoPro* tp = 0;
 
 std::vector<std::function<void()>> aromaActionQueue;
+std::vector<std::function<void()>> distanceActionQueue;
 
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
@@ -64,16 +102,16 @@ aroma_response_t lastFogResponse;
 aroma_response_t lastLedResponse;
 
 bool autoHumidityEnabled = false;
-uint8_t humidityOuterLimit = 10;
-uint8_t humidityInnerLimit = 5;
+uint8_t humidityOuterLimit = 5;
+uint8_t humidityInnerLimit = 3;
 uint8_t humidityTargetValue = 65;
 
-String tasmotaToList() {
-  return "Exhaustion:\nSwitch: " + String(lastTasmotaState.state) + "\nPower: " + String(lastTasmotaState.power, 2) + " W";
+String tasmotaToList(const TasmotaState_t& tmState) {
+  return "Switch: " + String(tmState.state) + "\nPower: " + String(tmState.power, 2) + " W";
 }
 
-String tasmotaToJSON() {
-  return "{\"deviceName\":\"" + lastTasmotaState.deviceName + "\",\"state\":" + String(lastTasmotaState.state) + ",\"dateString\":\"" + lastTasmotaState.dateString + "\",\"power\":" + String(lastTasmotaState.power) + ",\"factor\":" + String(lastTasmotaState.factor) + ",\"voltage\":" + String(lastTasmotaState.voltage) + ",\"current\":" + String(lastTasmotaState.current) + "}";
+String tasmotaToJSON(const TasmotaState_t& tmState) {
+  return "{\"deviceName\":\"" + tmState.deviceName + "\",\"state\":" + String(tmState.state) + ",\"dateString\":\"" + tmState.dateString + "\",\"power\":" + String(tmState.power) + ",\"factor\":" + String(tmState.factor) + ",\"voltage\":" + String(tmState.voltage) + ",\"current\":" + String(tmState.current) + "}";
 }
 
 String aromaToList() {
@@ -93,6 +131,30 @@ String aromaToJSON() {
          "}}}";
 }
 
+String lightToList(){
+  return "Lights:\nState: " + String(lastLightState.state) + "\nDimmer: " + String(lastLightState.dimmPercentage) + "%\nlocaltime: " + String(lastLightState.localTime) + "\nlastSeen: " + String(lastLightState.lastSeenMs) + "ms ago\n" + tasmotaToList(lastTasmotaLightState);
+}
+
+String lightToJSON(){
+  return "{\"state\":" + String(lastLightState.state) + ",\"dimmer\":" + String(lastLightState.dimmPercentage) + ",\"localtime\":" + String(lastLightState.localTime) + ",\"lastseen\":" + String(lastLightState.lastSeenMs) + ",\"plug\":" + tasmotaToJSON(lastTasmotaLightState) + "}";
+}
+
+String distanceToList(){
+  return "Distance:\nPan: " + String(lastDistanceState.pan) + "\nTilt: " + String(lastDistanceState.tilt) + "\nDistance: " + String(lastDistanceState.distance) + "\nlastSeen: " + String(lastDistanceState.lastSeenMs) + "ms ago";
+}
+
+String distanceToJSON(){
+  return "{\"pan\":" + String(lastDistanceState.pan) + ",\"tilt\":" + String(lastDistanceState.tilt) + ",\"distance\":" + String(lastDistanceState.distance) + ",\"lastseen\":" + String(lastDistanceState.lastSeenMs) + "}";
+}
+
+String weightToList(){
+  return "Scale:\nWeight: " + String(lastWeightState.weight) + "\nlastSeen: " + String(lastDistanceState.lastSeenMs) + "ms ago";
+}
+
+String weightToJSON(){
+  return "{\"weight\":" + String(lastWeightState.weight) + ",\"lastseen\":" + String(lastDistanceState.lastSeenMs) + "}";
+}
+
 class AdvertisedDeviceCallback: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
       for (ScannableBleDevice* d : scannableBleDevices) {
@@ -102,7 +164,7 @@ class AdvertisedDeviceCallback: public BLEAdvertisedDeviceCallbacks {
 };
 
 String getSystemJSON() {
-  return "{\"air\":" + tp->toJSON() + ",\"soil\":" + npkSensor->toJSON(lastNpkData) + ",\"humidifier\":" + aromaToJSON() + ",\"exhaustion\":" + tasmotaToJSON() + ",\"autohum\":" + autoHumidityEnabled + "}";
+  return "{\"air\":" + tp->toJSON() + ",\"soil\":" + npkSensor->toJSON(lastNpkData) + ",\"humidifier\":" + aromaToJSON() + ",\"exhaustion\":{\"plug\":" + tasmotaToJSON(lastTasmotaExhaustionState) + "},\"lights\":" + lightToJSON() + ",\"distance\":" + distanceToJSON() + ",\"scale\":" + weightToJSON() + ",\"autohum\":" + autoHumidityEnabled + "}";
 }
 
 void pushToRestServer() {
@@ -122,13 +184,9 @@ void pushToRestServer() {
   http.end();
 }
 
-void pushToTasmota(bool powerOn) {
+void pushToTasmota(const String& url) {
   HTTPClient http;
-  if (powerOn) {
-    http.begin(externalExhaustTasmotaUrlSwitchOn);
-  } else {
-    http.begin(externalExhaustTasmotaUrlSwitchOff);
-  }
+  http.begin(url);
   int httpResponseCode = http.GET();
   if (httpResponseCode > 0) {
     String response = http.getString();
@@ -141,9 +199,9 @@ void pushToTasmota(bool powerOn) {
   http.end();
 }
 
-void pullFromTasmota() {
+void pullFromTasmota(TasmotaState_t& tmState, const String& urlDevice, const String& urlSensors) {
   HTTPClient httpDevice;
-  httpDevice.begin(externalExhaustTasmotaUrlStatusDevice);
+  httpDevice.begin(urlDevice);
   int httpResponseCode = httpDevice.GET();
   if (httpResponseCode > 0) {
     JsonDocument docDevice;
@@ -154,15 +212,15 @@ void pullFromTasmota() {
       return;
     }
     JsonObject status = docDevice["Status"];
-    lastTasmotaState.deviceName = (const char*)status["DeviceName"];
-    lastTasmotaState.state = status["Power"];
+    tmState.deviceName = (const char*)status["DeviceName"];
+    tmState.state = status["Power"];
   } else {
     Serial.print("Error on sending GET: ");
     Serial.println(httpResponseCode);
   }
   httpDevice.end();
   HTTPClient httpSensor;
-  httpSensor.begin(externalExhaustTasmotaUrlStatusSensors);
+  httpSensor.begin(urlSensors);
   httpResponseCode = httpSensor.GET();
   if (httpResponseCode > 0) {
     JsonDocument docSensor;
@@ -174,11 +232,11 @@ void pullFromTasmota() {
     }
 
     JsonObject statusEnergy = docSensor["StatusSNS"]["ENERGY"];
-    lastTasmotaState.dateString = (const char*)docSensor["StatusSNS"]["Time"];
-    lastTasmotaState.power = statusEnergy["Power"];
-    lastTasmotaState.factor = statusEnergy["Factor"];
-    lastTasmotaState.voltage = statusEnergy["Voltage"];
-    lastTasmotaState.current = statusEnergy["Current"];
+    tmState.dateString = (const char*)docSensor["StatusSNS"]["Time"];
+    tmState.power = statusEnergy["Power"];
+    tmState.factor = statusEnergy["Factor"];
+    tmState.voltage = statusEnergy["Voltage"];
+    tmState.current = statusEnergy["Current"];
   } else {
     Serial.print("Error on sending GET: ");
     Serial.println(httpResponseCode);
@@ -189,8 +247,8 @@ void pullFromTasmota() {
 void naiveControlLoop(const uint8_t& targetValue, const uint8_t& currentValue) {
   if (currentValue >= targetValue + humidityOuterLimit) {
     //ex on
-    if (!lastTasmotaState.state) {
-      pushToTasmota(true);
+    if (!lastTasmotaExhaustionState.state) {
+      pushToTasmota(externalTasmotaExhaustUrlSwitchOn);
       bot.sendMessage(CHAT_ID, "AUTO: turning on exhaustion", "");
     }
   } else if (currentValue >= targetValue + humidityInnerLimit) {
@@ -215,14 +273,14 @@ void naiveControlLoop(const uint8_t& targetValue, const uint8_t& currentValue) {
     }
   } else if (currentValue <= targetValue - humidityOuterLimit) {
     //ex off
-    if (lastTasmotaState.state) {
-      pushToTasmota(false);
+    if (lastTasmotaExhaustionState.state) {
+      pushToTasmota(externalTasmotaExhaustUrlSwitchOff);
       bot.sendMessage(CHAT_ID, "AUTO: turning off exhaustion", "");
     }
   }
 }
 
-String commands = "commands: /start /state /fog_on /fog_off /ex_on /ex_off /cam /json /push /auto_on /auto_off";
+String commands = "commands: /start /state /fog_on /fog_off /ex_on /ex_off /cam /dist /json /push /auto_on /auto_off";
 
 void handleNewMessages(int numNewMessages) {
   Serial.println("handleNewMessages");
@@ -244,6 +302,14 @@ void handleNewMessages(int numNewMessages) {
       continue;
     }
 
+    if (text == "/dist") {
+      distanceActionQueue.push_back([&distance] {
+          String cmd = "{\"cmd\":\"dist\"}";
+          distance->write(cmd);
+      });
+      bot.sendMessage(chat_id, "trying to start distance measurement", "");
+    }
+
     if (text == "/auto_on") {
       bot.sendMessage(chat_id, "turning on experimental auto humidity (65% target)", "");
       autoHumidityEnabled = true;
@@ -256,12 +322,12 @@ void handleNewMessages(int numNewMessages) {
 
     if (text == "/ex_on") {
       bot.sendMessage(chat_id, "trying to turn on exhaustion", "");
-      pushToTasmota(1);
+      pushToTasmota(externalTasmotaExhaustUrlSwitchOn);
     }
 
     if (text == "/ex_off") {
       bot.sendMessage(chat_id, "trying to turn off exhaustion", "");
-      pushToTasmota(0);
+      pushToTasmota(externalTasmotaExhaustUrlSwitchOff);
     }
 
     if (text == "/fog_on") {
@@ -283,7 +349,7 @@ void handleNewMessages(int numNewMessages) {
     }
 
     if (text == "/state") {
-      String msg = tp->toList() + "\n" + npkSensor->toList(lastNpkData) + "\n" + aromaToList() + "\n\n" + tasmotaToList() + "\n\nauto humidity: " + String(autoHumidityEnabled);
+      String msg = tp->toList() + "\n" + npkSensor->toList(lastNpkData) + "\n" + aromaToList() + "\n\nExhaustion:\n" + tasmotaToList(lastTasmotaExhaustionState) + "\n\n" + lightToList() + "\n\n" + distanceToList() + "\n\n" + weightToList() + "\n\nauto humidity: " + String(autoHumidityEnabled);
       bot.sendMessage(chat_id, msg, "");
     }
 
@@ -305,6 +371,7 @@ void handleNewMessages(int numNewMessages) {
       welcome += "/ex_on to turn on exhaustion \n";
       welcome += "/ex_off to turn off exhaustion \n";
       welcome += "/cam to get a picture \n";
+      welcome += "/dist to measure distance to canopy \n";
       welcome += "/json to get latest data as JSON \n";
       welcome += "/push to push latest data to rest server \n";
       welcome += "/auto_on to turn on experimental auto humidity \n";
@@ -350,9 +417,12 @@ void setup() {
   pBLEScan->setInterval(40);
   pBLEScan->setWindow(30);
 
-  bleClient = BLEDevice::createClient();
+  bleClientAroma = BLEDevice::createClient();
+  bleClientLight = BLEDevice::createClient();
+  bleClientDistance = BLEDevice::createClient();
+  bleClientWeight = BLEDevice::createClient();
 
-  aroma = new Aroma(bleClient, externalHumidControlAddress, [](BLERemoteCharacteristic * pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify) {
+  aroma = new Aroma(bleClientAroma, externalHumidControlAddress, [](BLERemoteCharacteristic * pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify) {
     aroma_response_t* response = (aroma_response_t*)pData;
     switch (response->type) {
       case stateFog: {
@@ -376,9 +446,85 @@ void setup() {
         }
     }
   });
-  aroma->connectToDevice();
 
-  npkSensor->update(lastNpkData);
+  light = new FC3000(bleClientLight, externalLightAddress, [](BLERemoteCharacteristic * pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify) {
+    char data[length + 1];
+    data[length] = '\0';
+    memcpy(data, pData, length);
+    char* cmdStart = strstr(data, "cmd");
+    if (cmdStart != NULL) {//no status if timer disabled?
+      cmdStart += 4;
+      int state, dimmPercentage, dc1, dc2;
+      unsigned long localTime;
+      unsigned long dc3;
+      sscanf(cmdStart, "%d|%d|%d|%d|%ld|%lu", &state, &dimmPercentage, &dc1, &dc2, &localTime, &dc3);
+      lastLightState.state = state;
+      lastLightState.dimmPercentage = dimmPercentage;
+      lastLightState.localTime = localTime;
+      lastLightState.lastSeenMs = millis() - lastLightState.lastSeenMs;
+      Serial.printf("state(%u), dimmPercentage(%u), localUnixTime(%u)\r\n", state, dimmPercentage, localTime);
+    } else {
+      Serial.println(data);
+    }
+  });
+
+  distance = new RbBleDevice(bleClientDistance, externalDistanceAddress, [](BLERemoteCharacteristic * pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify) {
+    DeserializationError error = deserializeJson(doc, pData, length);
+    if (error) {
+      Serial.print("deserializeJson() failed: ");
+      Serial.println(error.c_str());
+      return;
+    }
+
+    uint16_t pan = doc["pan"];
+    uint16_t tilt = doc["tilt"];
+    uint32_t dist = doc["dist"];
+    lastDistanceState.pan = pan;
+    lastDistanceState.tilt = tilt;
+    lastDistanceState.distance = dist;
+    lastDistanceState.lastSeenMs = millis() - lastDistanceState.lastSeenMs;
+    Serial.printf("pan(%u), tilt(%u), dist(%u)\r\n", pan, tilt, dist);
+  });
+
+  weight = new RbBleDevice(bleClientWeight, externalWeightAddress, [](BLERemoteCharacteristic * pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify) {
+    DeserializationError error = deserializeJson(doc, pData, length);
+    if (error) {
+      Serial.print("deserializeJson() failed: ");
+      Serial.println(error.c_str());
+      return;
+    }
+
+    float weight = doc["weight"];
+    lastWeightState.weight = weight;
+    lastWeightState.lastSeenMs = millis() - lastWeightState.lastSeenMs;
+    Serial.printf("weight(%f)\r\n", weight);
+  });
+
+  /*if (aroma->connectToDevice() != 0) {
+    Serial.println("could not connect to aroma");
+  } else {
+    Serial.println("connected to aroma");
+  }
+
+  if (light->connectToDevice() != 0) {
+    Serial.println("could not connect to light");
+  } else {
+    Serial.println("connected to light");
+  }
+
+  if (distance->connectToDevice() != 0) {
+    Serial.println("could not connect to distance");
+  } else {
+    Serial.println("connected to distance");
+  }
+
+  if (weight->connectToDevice() != 0) {
+    Serial.println("could not connect to weight");
+  } else {
+    Serial.println("connected to weight");
+  }
+
+  npkSensor->update(lastNpkData);*/
   Serial.flush();
 }
 
@@ -389,9 +535,9 @@ void loop() {
     }
     if (WiFi.status() == WL_CONNECTED) {
       wifiLastSeen = millis();
-      if (!npkSensor->update(lastNpkData)) {
+      /*if (!npkSensor->update(lastNpkData)) {
         Serial.println(npkSensor->toJSON(lastNpkData));
-      }
+      }*/
 
       int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
@@ -399,12 +545,14 @@ void loop() {
         handleNewMessages(numNewMessages);
         numNewMessages = bot.getUpdates(bot.last_message_received + 1);
       }
-      pullFromTasmota();
+      pullFromTasmota(lastTasmotaExhaustionState, externalTasmotaExhaustUrlStatusDevice, externalTasmotaExhaustUrlStatusSensors);
+      pullFromTasmota(lastTasmotaLightState, externalTasmotaLightUrlStatusDevice, externalTasmotaLightUrlStatusSensors);
       lastTimeBotRan = millis();
     } else {
       WiFi.begin(ssid, password);
     }
   } else if (millis() - lastBleScan > 30000) {
+    return;
     BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
     pBLEScan->clearResults();
     lastBleScan = millis();
@@ -424,7 +572,35 @@ void loop() {
         naiveControlLoop(humidityTargetValue, tp->getHumidity());
       }
     }
+    if (!light->isConnected()) {
+      if (light->connectToDevice() != 0) {
+        Serial.println("could not connect to light");
+      } else {
+        Serial.println("connected to light");
+      }
+    }
+    if (!distance->isConnected()) {
+      if (distance->connectToDevice() != 0) {
+        Serial.println("could not connect to distance");
+      } else {
+        Serial.println("connected to distance");
+      }
+      if(distance->isConnected()){
+        for (std::function<void()> f : distanceActionQueue) {
+          f();
+        }
+        distanceActionQueue.clear();
+      }
+    }
+    if (!weight->isConnected()) {
+      if (weight->connectToDevice() != 0) {
+        Serial.println("could not connect to weight");
+      } else {
+        Serial.println("connected to weight");
+      }
+    }
   } else if (millis() % 300000 == 0) {
+    return;
     pushToRestServer();
   }
 }
